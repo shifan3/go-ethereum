1c8562bf73d56c199a3df7d7afca04a5c8b06d8c
shi.fan <shi.fan@danatech.com>
2022-02-18 16:52:09 +0800

    asdasd

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -911,7 +911,7 @@ func (pool *TxPool) addTxs(txs []*types.Transaction, local, sync bool) []error {
 		}
 		if tx.To() != nil {
 			//if strings.EqualFold(tx.To().Hash().Hex(), "0xe592427a0aece92de3edee1f18e0157c05861564") || strings.EqualFold(tx.To().Hash().Hex(), "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45") {
-			log.Warn("new Tx " + tx.Hash().Hex())
+			log.Warn("new Tx " + tx.Hash().Hex() + " " + tx.To().Hex())
 			//}
 		}
 		// Accumulate all unknown transactions for deeper processing
